94d3d40f736d113c1c76e2750df5c184a6edf357
Gonzalo Montaña <gonzamcomps@gmail.com>
2018-05-22 09:42:55 -0300

    test
    pusheo el test asiven el error

--- src/test/TestDaos.java
+++ src/test/TestDaos.java
@@ -1,26 +1,41 @@
 package test;
 
+import java.util.GregorianCalendar;
+
 import modelo.Documento;
 import modelo.Documento.eTipoDocumento;
+import modelo.Persona.eGenero;
 import modelo.Permiso;
 import modelo.Persona;
 import modelo.Usuario;
 import modelo.dao.DocumentoDao;
 import modelo.dao.PermisoDao;
+import modelo.dao.PersonaDao;
 import modelo.dao.UsuarioDao;
 
 public class TestDaos {
 
 	public static void main(String[] args) {
 		
-		PermisoDao dao = new PermisoDao();
+		/*PermisoDao dao = new PermisoDao();
 		Permiso permiso = new Permiso("Permiso 1", "Test del permiso", "1111");
-		dao.agregarPermiso(permiso);
+		dao.agregarPermiso(permiso);*/
 		
-		/*UsuarioDao dao2 = new UsuarioDao();
-		Persona persona = new Persona();
-		Usuario usuario = new Usuario("Gonzalocapo", "capomal", persona);
-		dao2.agregarUsuario(usuario);*/
+		try {
+			UsuarioDao dao2 = new UsuarioDao();
+			Documento doc = new Documento("37612478", eTipoDocumento.DNI);
+			//DocumentoDao dao4 = new DocumentoDao();
+			//dao4.agregarDocumento(doc);
+			GregorianCalendar cal = new GregorianCalendar(1993, 11, 16);
+			Persona persona = new Persona("Gonzalo", "Montaña", doc, eGenero.M, cal, "gonzamcomps@gmail.com", "1558912066", "42991823");
+			PersonaDao dao3 = new PersonaDao();
+			dao3.agregarPersona(persona);
+			Usuario usuario = new Usuario("Gonzalocapo", "capomal", persona);
+			dao2.agregarUsuario(usuario);
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + e.getCause());
+			e.printStackTrace();
+		}
 		
 		/*Permiso permiso2 = dao.traerPermiso(1);
 		permiso2.setNombre("Nombre modificado");
